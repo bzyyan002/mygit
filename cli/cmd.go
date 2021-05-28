haydenwang/lotus

Add verifreg utils to CLI

cli/cmd.go
@@ -71,6 +71,7 @@ var Commands = []*cli.Command{
 	WithCategory("basic", walletCmd),
 	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
+	WithCategory("basic", verifRegCmd),
 	WithCategory("basic", paychCmd),
 	WithCategory("developer", AuthCmd),
 	WithCategory("developer", MpoolCmd),

bzyyan002/mygit

cli/cmd.go
@@ -70,6 +70,7 @@ var Commands = []*cli.Command{
 	WithCategory("basic", walletCmd),
 	WithCategory("basic", clientCmd),
 	WithCategory("basic", multisigCmd),
+	WithCategory("basic", filplusCmd),
 	WithCategory("basic", paychCmd),
 	WithCategory("developer", AuthCmd),
 	WithCategory("developer", MpoolCmd),